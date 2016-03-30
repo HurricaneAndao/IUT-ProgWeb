e81bb3ac8e9933f9aeed6bce5aa7b74cd45f5288
HurricaneAndao <hurricane_andao@laposte.net>
2016-03-30 20:48:38 +0200

    Update connectDb.php

--- connectDb.php
+++ connectDb.php
@@ -2,9 +2,9 @@
 /* Fonction de connexion en local à la base de données*/
  function connectDb(){
       $host="localhost"; // ou sql.hebergeur.com
-      $user="p1512799";      // ou login
-      $password="248128";      // mot de passe
-      $dbname="p1512799";
+      $user="";      // ou login
+      $password="";      // mot de passe
+      $dbname="";
   try {
        $bdd=new PDO('mysql:host='.$host.';dbname='.$dbname.
                     ';charset=utf8',$user,$password);
